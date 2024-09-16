26bd0ff27b591c494f5e8ab6cfddbb24a0bfc196
tpm909 <ankitoperez909@gmail.com>
2024-09-16 15:50:51 -0300

    lista de rubro

--- src/main/java/jFrame/BusquedaPorRubro.java
+++ src/main/java/jFrame/BusquedaPorRubro.java
@@ -4,6 +4,7 @@
  */
 package jFrame;
 
+import clases.Producto;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -39,7 +40,7 @@ public class BusquedaPorRubro extends javax.swing.JInternalFrame {
         jLLetras = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTablexRubro = new javax.swing.JTable();
-        jCRubro1 = new javax.swing.JComboBox<>();
+        Rubro = new javax.swing.JComboBox<>();
 
         jLTituloxRubro.setText("PRODUCTOS POR RUBRO");
 
@@ -53,12 +54,25 @@ public class BusquedaPorRubro extends javax.swing.JInternalFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Title ", "Title 2", "Title 3", "Title 4"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(jTablexRubro);
 
-        jCRubro1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        Rubro.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Limpieza", "Comestible", "Perfumeria" }));
+        Rubro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RubroActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -73,7 +87,7 @@ public class BusquedaPorRubro extends javax.swing.JInternalFrame {
                         .addContainerGap()
                         .addComponent(jLLetras)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jCRubro1, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(Rubro, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 371, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -87,7 +101,7 @@ public class BusquedaPorRubro extends javax.swing.JInternalFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLLetras)
-                    .addComponent(jCRubro1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(Rubro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
                 .addContainerGap())
@@ -96,9 +110,29 @@ public class BusquedaPorRubro extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void RubroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RubroActionPerformed
+       borrarFilas();
+       for (Producto prod : MenuPrincipal.listaProductos) {
+            if (prod.getRubro()==Rubro.getSelectedItem()) { // Compara ignorando mayúsculas/minúsculas.
+                modelo.addRow(new Object[]{
+                    prod.getCodigo(),
+                    prod.getDescripcion(),
+                    prod.getStock(),
+                    prod.getPrecio(),
+                    prod.getRubro(),
+                });
+            }
+        }
+       
+       
+       
+        
+        
+    }//GEN-LAST:event_RubroActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<String> jCRubro1;
+    private javax.swing.JComboBox<String> Rubro;
     private javax.swing.JLabel jLLetras;
     private javax.swing.JLabel jLTituloxRubro;
     private javax.swing.JScrollPane jScrollPane1;
